8980c4bb5f2bf30edffe3fc79e7ee93980011fb5
CaMel <hackball08@gmail.com>
2023-05-25 13:30:30 +0700

    fixed event card

--- components/event_card.tsx
+++ components/event_card.tsx
@@ -23,6 +23,7 @@ export default function EventCard({
             />
           }
           {
+            !img &&
             <div className="flex w-full h-full bg-gradient-to-r from-[#b7175c] to-[#E90064] justify-center items-center text-center">
               <h1 className="text-white font-bold text-4xl rotate-[-12deg] skew-x-12">
                 {eventName}
