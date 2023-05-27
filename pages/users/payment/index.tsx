b33cd393dd3d61076bacd989498aa66546b7a3e6
CaMel <hackball08@gmail.com>
2023-05-27 14:51:39 +0700

    feature delete payment

--- pages/users/payment/index.tsx
+++ pages/users/payment/index.tsx
@@ -1,21 +1,68 @@
 import MenuBar from "@/components/account_menu";
 import ProfileCard from "@/components/profile_card";
 import TabBar from "@/components/show_ticket";
-import React from "react";
-import { getSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import { getSession, useSession } from "next-auth/react";
 import { GetServerSideProps } from "next";
 import axios from "axios";
-import { Button } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { useRouter } from "next/router";
-
-export default function PaymentMethod({userPayment}) {
+import DeleteIcon from '@mui/icons-material/Delete';
+type PaymentInfo = {
+  payment_info_id: number;
+  card_id: string;
+  payment_name: string;
+  payment_method: string;
+  prompt_pay: number;
+  expired_date: string;
+  user_id: number;
+}
+export default function PaymentMethod() {
+  const [userPayment, setUserPayment] = useState<PaymentInfo[]>([])
+  const [open, setOpen] = useState(false)
+  const session = useSession()
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const censorValue = (value: string) => {
     const visibleDigits = 4; // Number of visible digits at the end
     const maskedValue = '*'.repeat(value.length - visibleDigits) + value.slice(-visibleDigits);
     return maskedValue;
   };
   const router = useRouter()
+  useEffect(() => {
+    if(session.data?.user.user_id){
+      console.log(session.data?.user.user_id)
+      const user_id = session.data?.user.user_id;
+  
+      try {
+        axios.get(
+          `https://ticketapi.fly.dev/get_user_payment?user_id=${user_id}`
+        ).then((res)=>{
+          // console.log(res)
+          setUserPayment(res.data)
+        })
+        // setUserPayment(userPayment)
+      } catch (error) {
+        console.log(error)        
+      }
+
+    }
+    
+  }, [])
+  
   // console.log(userPayment)
+  const handleDelete = (payment_info_id: number) =>{
+    axios.delete(`https://ticketapi.fly.dev/delete_user_payment?payment_info_id=${payment_info_id}`).then(()=>{
+      setUserPayment(userPayment => userPayment.filter(obj => obj.payment_info_id !== payment_info_id))
+      handleClose()
+      }
+    )
+  }
+
   return (
     <main>
       <div className="container mx-auto px-10">
@@ -41,26 +88,78 @@ export default function PaymentMethod({userPayment}) {
               {userPayment && userPayment.map((payment)=>{
                 if(payment.payment_method == "Credit Card"){
                   return (
-                      <div className="flex shadow-lg bg-gray-100 p-4 w-full rounded-md mb-4">
-                        <img className="mr-10" src="/masterCardIcon.svg" alt="" width={100} height={100} />
-                        <div className="flex flex-col">
-                          <h4 className="text-sm font-light">Credit/Debit Card</h4>
-                          <h2 className="text-3xl font-semibold text-[#060047]">{payment.payment_name}</h2>
-                          <h3 className="text-xl">{censorValue(payment.card_id.toString())}</h3>
-                          <h3 className="text-sm">expired date: {payment.expired_date.split('T')[0]}</h3>
+                      <div className="flex shadow-lg bg-gray-100 p-4 w-full rounded-md mb-4 justify-between">
+                        <Dialog
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="alert-dialog-title"
+                          aria-describedby="alert-dialog-description"
+                        >
+                          <DialogTitle id="alert-dialog-title">
+                            {"Delete this payment information?"}
+                          </DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              delete this payment information permarnently
+                            </DialogContentText>
+                          </DialogContent>
+                          <DialogActions>
+                            <Button onClick={handleClose}>Cancle</Button>
+                            <Button onClick={()=>handleDelete(payment.payment_info_id)} autoFocus style={{ color: '#E90064' }}>
+                              Delete
+                            </Button>
+                          </DialogActions>
+                        </Dialog>
+                        <div className="flex">
+                          <img className="mr-12" src="/masterCardIcon.svg" alt="" width={100} height={100} />
+                          <div className="flex flex-col">
+                            <h4 className="text-sm font-light">Credit/Debit Card</h4>
+                            <h2 className="text-3xl font-semibold text-[#060047]">{payment.payment_name}</h2>
+                            <h3 className="text-xl">{censorValue(payment.card_id.toString())}</h3>
+                            <h3 className="text-sm">expired date: {payment.expired_date.split('T')[0]}</h3>
+                          </div>
                         </div>
+                        <Button onClick={handleClickOpen} variant="text" startIcon={<DeleteIcon />} style={{ color: '#E90064' }}>
+                          Delete
+                        </Button>
                       </div>
                   )
                 }
                 if(payment.payment_method == "Prompt Pay"){
                   return (
-                      <div className="flex shadow-lg bg-gray-100 p-4 w-full rounded-md mb-4">
-                        <img className="mr-10" src="/promptPayIcon.svg" alt="" width={100} height={100} />
-                        <div className="flex flex-col">
-                          <h4 className="text-sm font-light">Prompt Pay</h4>
-                          <h2 className="text-3xl font-semibold text-[#060047]">{payment.payment_name}</h2>
-                          <h3 className="text-xl">{censorValue(payment.prompt_pay.toString())}</h3>
+                      <div className="flex shadow-lg bg-gray-100 p-4 w-full rounded-md mb-4 justify-between">
+                        <Dialog
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="alert-dialog-title"
+                          aria-describedby="alert-dialog-description"
+                        >
+                          <DialogTitle id="alert-dialog-title">
+                            {"Delete this payment information?"}
+                          </DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              delete this payment information permarnently
+                            </DialogContentText>
+                          </DialogContent>
+                          <DialogActions>
+                            <Button onClick={handleClose}>Cancle</Button>
+                            <Button onClick={()=>handleDelete(payment.payment_info_id)} autoFocus style={{ color: '#E90064' }}>
+                              Delete
+                            </Button>
+                          </DialogActions>
+                        </Dialog>
+                        <div className="flex">
+                          <img className="mr-12" src="/promptPayIcon.svg" alt="" width={100} height={100} />
+                          <div className="flex flex-col">
+                            <h4 className="text-sm font-light">Prompt Pay</h4>
+                            <h2 className="text-3xl font-semibold text-[#060047]">{payment.payment_name}</h2>
+                            <h3 className="text-xl">{censorValue(payment.prompt_pay.toString())}</h3>
+                          </div>
                         </div>
+                        <Button onClick={handleClickOpen} variant="text" startIcon={<DeleteIcon />} style={{ color: '#E90064' }}>
+                          Delete
+                        </Button>
                       </div>
                   )
                 }
@@ -73,30 +172,8 @@ export default function PaymentMethod({userPayment}) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  // console.log("test", session)
-  const user_id = session?.user?.user_id;
-  // console.log(user_id)
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSession(context);
+//   // console.log("test", session)
 
-  try {
-    const response = await axios.get(
-      `https://ticketapi.fly.dev/get_user_payment?user_id=${user_id}`
-    );
-    const userPayment = response.data;
-
-    return {
-      props: {
-        userPayment,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-
-    return {
-      props: {
-        userPayment: null, // or handle error case as desired
-      },
-    };
-  }
-};
+// };
