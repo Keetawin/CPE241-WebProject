6bf20a2a6d2f214bf3c8755acc9fe354fdfc2a7d
Keetawin <khitawanlimxrun@gmail.com>
2023-05-29 13:27:27 +0700

    add:some dash api

--- pages/organize/[id]/dashboard/[eventId].tsx
+++ pages/organize/[id]/dashboard/[eventId].tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
+import { getSession } from "next-auth/react";
 import {
   Alert,
   Checkbox,
@@ -16,47 +17,40 @@ import {
   Select,
   TextField,
 } from "@mui/material";
-import { useState, useEffect, Fragment } from "react";
-import { Bar } from "react-chartjs-2";
-import { Chart, CategoryScale, LinearScale, BarElement } from "chart.js";
+import { useState, useEffect, Fragment, useRef } from "react";
+import { Bar, Pie } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+} from "chart.js";
 import axios from "axios";
 import { CircularProgress } from "@mui/material";
 import dayjs from "dayjs";
 
 // Register the necessary elements and scales
-Chart.register(CategoryScale, LinearScale, BarElement);
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
+Chart.register(CategoryScale, LinearScale, BarElement, ArcElement);
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+interface EventData {
+  event_id: number;
+  organize_id: number;
+  event_name: string;
+  event_startdate: string;
+  event_enddate: string;
+  location: string;
+  email: string | null;
+  tel: string | null;
+  website: string | null;
+  approved: boolean;
+  payment_info_id: number;
+  event_description: string;
+  poster: string;
+  event_type_id: number;
+  total_refund: string;
+  total_sale: string;
+}
 
 export default function EventDashBoard() {
   const router = useRouter();
@@ -68,6 +62,26 @@ export default function EventDashBoard() {
   const [seatTypes, setSeatTypes] = useState([]);
   const [isSecondModalOpen, setIsSecondModalOpen] = useState(false);
   const [availableSeats, setAvailableSeats] = useState([]);
+  const [ageDistribution, setAgeDistribution] = useState([]);
+  const [genderDistribution, setGenderDistribution] = useState([]);
+  const [eventData2, setEventData2] = useState<EventData[]>([]);
+  const [getTable, setGetTable] = useState([]);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<EventData[]>(
+          "https://ticketapi.fly.dev/get_event_sale"
+        );
+        setEventData2(response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const [formData, setFormData] = useState({
     event_id: eventId,
@@ -84,6 +98,92 @@ export default function EventDashBoard() {
     seat_start: "",
     seat_end: "",
   });
+
+  const transformDataForChart = () => {
+    return ageDistribution.map((data) => ({
+      ageRange: `Age ${data.age}`,
+      frequency: data.frequency,
+    }));
+  };
+
+  const transformedData = transformDataForChart();
+
+  const labels = transformedData.map((data) => data.ageRange);
+  const frequencies = transformedData.map((data) => data.frequency);
+
+  const backgroundColors = [
+    "rgba(255, 99, 132, 0.2)",
+    "rgba(54, 162, 235, 0.2)",
+    "rgba(255, 205, 86, 0.2)",
+    "rgba(75, 192, 192, 0.2)",
+  ];
+
+  const borderColor = [
+    "rgb(255, 99, 132)",
+    "rgb(54, 162, 235)",
+    "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)",
+  ];
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: frequencies,
+        backgroundColor: backgroundColors,
+        borderColor: borderColor,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const label = labels[context.dataIndex];
+            const value = context.parsed;
+            return `${label}: ${value}`;
+          },
+        },
+      },
+    },
+  };
+
+  const chartData2 = {
+    labels: genderDistribution.map((data) => data.gender),
+    datasets: [
+      {
+        data: genderDistribution.map((data) => data.gender_count),
+        backgroundColor: backgroundColors,
+        borderColor: borderColor,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartOptions2 = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const label = genderDistribution[context.dataIndex];
+            const value = context.parsed;
+            return `${label}: ${value}`;
+          },
+        },
+      },
+    },
+  };
 
   useEffect(() => {
     const fetchAvailableSeats = async () => {
@@ -236,6 +336,76 @@ export default function EventDashBoard() {
     }
   }, [eventId]);
 
+  useEffect(() => {
+    const fetchGenderDistribution = async () => {
+      try {
+        const response = await axios.get(
+          `https://ticketapi.fly.dev/event_gender_distribute?event_id=${eventId}`
+        );
+        setGenderDistribution(response.data);
+      } catch (error) {
+        console.error("Error while fetching gender distribution data:", error);
+      }
+    };
+
+    // Fetch gender distribution data when eventId is available
+    if (eventId) {
+      fetchGenderDistribution();
+    }
+  }, [eventId]);
+
+  useEffect(() => {
+    const fetchTransaction = async () => {
+      try {
+        const response = await axios.get(
+          `https://ticketapi.fly.dev/event_transaction?event_id=${eventId}`
+        );
+        setGetTable(response.data);
+      } catch (error) {
+        console.error("Error while fetching data:", error);
+      }
+    };
+
+    // Fetch gender distribution data when eventId is available
+    if (eventId) {
+      fetchTransaction();
+    }
+  }, [eventId]);
+
+  useEffect(() => {
+    const mapAgeToRange = (age) => {
+      if (age < 18) {
+        return "Teenager";
+      } else if (age >= 18 && age < 30) {
+        return "Young Adult";
+      } else if (age >= 30 && age < 60) {
+        return "Adult";
+      } else {
+        return "Senior";
+      }
+    };
+
+    const fetchAgeDistribution = async () => {
+      try {
+        const response = await axios.get(
+          `https://ticketapi.fly.dev/event_age_distribute?event_id=${eventId}`
+        );
+        const ageDistributionData = response.data.map((data) => ({
+          ageRange: mapAgeToRange(data.age),
+          frequency: data.frequency,
+        }));
+        setAgeDistribution(ageDistributionData);
+      } catch (error) {
+        console.error("Error while fetching age distribution data:", error);
+      }
+    };
+
+    // Fetch age distribution data when eventId is available
+    if (eventId) {
+      fetchAgeDistribution();
+    }
+  }, [eventId]);
+
   const data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
@@ -312,44 +482,26 @@ export default function EventDashBoard() {
               </div>
             </div>
 
-            <div className="grid grid-cols-3 gap-8 w-full h-full py-8 ">
-              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
-                <div className="flex justify-center px-4 flex-col gap-6 ">
-                  <p className=" text-lg font-semibold">Total Ticket</p>
-                  <p className=" font-medium text-lg">Lorem</p>
-                  <p>Percent</p>
-                </div>
-                <div>Graph</div>
-              </div>
-
-              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
-                <div className="flex justify-center px-4 flex-col gap-6 ">
-                  <p className=" text-lg font-semibold">Total Follower</p>
-                  <p className=" font-medium text-lg">Lorem</p>
-                  <p>Percent</p>
-                </div>
-                <div>Graph</div>
-              </div>
-
-              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
-                <div className="flex justify-center px-4 flex-col gap-6 ">
-                  <p className=" text-lg font-semibold">Age</p>
-                  <p className=" font-medium text-lg">Lorem</p>
-                  <p>Percent</p>
-                </div>
-                <div>Graph</div>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-3 gap-8 w-full h-full ">
+            <div className="grid grid-cols-3 gap-8 w-full h-full mt-8 ">
               <div className="flex gap-6">
                 <div className="text-4xl text-green-500">
                   <FaMoneyBill />
                 </div>
                 <div className="flex flex-col gap-2">
                   <p className="text-sm text-gray-500">Earning</p>
-                  <p className=" text-xl font-semibold">Total</p>
-                  <p className="text-sm">37.5 % this month</p>
+                  <p className=" text-xl font-semibold">
+                    <div>
+                      {eventData2.map((event) => {
+                        if (event.event_id === Number(eventId)) {
+                          return (
+                            <p key={event.event_id}>{event.total_sale} ฿</p>
+                          );
+                        } else {
+                          return null;
+                        }
+                      })}
+                    </div>
+                  </p>
                 </div>
               </div>
 
@@ -359,8 +511,19 @@ export default function EventDashBoard() {
                 </div>
                 <div className="flex flex-col gap-2">
                   <p className="text-sm text-gray-500">Refund</p>
-                  <p className=" text-xl font-semibold">Total</p>
-                  <p className="text-sm">37.5 % this month</p>
+                  <p className=" text-xl font-semibold">
+                    <div>
+                      {eventData2.map((event) => {
+                        if (event.event_id === Number(eventId)) {
+                          return (
+                            <p key={event.event_id}>{event.total_refund} ฿</p>
+                          );
+                        } else {
+                          return null;
+                        }
+                      })}
+                    </div>
+                  </p>
                 </div>
               </div>
 
@@ -370,27 +533,92 @@ export default function EventDashBoard() {
                 </div>
                 <div className="flex flex-col gap-2">
                   <p className="text-sm text-gray-500">Total Sales</p>
-                  <p className=" text-xl font-semibold">Total</p>
-                  <p className="text-sm">37.5 % this month</p>
+                  <p className=" text-xl font-semibold">
+                    <div>
+                      {eventData2.map((event) => {
+                        if (event.event_id === Number(eventId)) {
+                          const calculatedValue =
+                            event.total_sale - event.total_refund;
+                          return (
+                            <p key={event.event_id}>{calculatedValue} ฿</p>
+                          );
+                        } else {
+                          return null;
+                        }
+                      })}
+                    </div>
+                  </p>
                 </div>
               </div>
             </div>
+
+            <div className="grid grid-cols-3 gap-8 w-full h-full py-8 ">
+              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
+                <div className="flex justify-center px-4 flex-col gap-6 ">
+                  <p className=" text-lg font-semibold">Total Ticket</p>
+                  <p className=" font-medium text-lg">Lorem</p>
+                  <p>Percent</p>
+                </div>
+                <div className="flex  px-4 flex-col py-8"></div>
+              </div>
+
+              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
+                <div className="flex justify-center px-4 flex-col gap-6 ">
+                  <p className=" text-lg font-semibold">Sex</p>
+                </div>
+                <div className="flex justify-center w-full">
+                  <Pie
+                    className="w-20 h-20 py-2"
+                    data={chartData2}
+                    options={chartOptions2}
+                  />
+                </div>
+              </div>
+
+              <div className="w-full flex h-48 rounded-lg border-[1.5px] ">
+                <div className="flex justify-center px-4 flex-col gap-6 ">
+                  <p className=" text-lg font-semibold">Age</p>
+                </div>
+                <div className="flex justify-center w-full">
+                  <Pie
+                    className="w-20 h-20 py-2"
+                    data={chartData}
+                    options={chartOptions}
+                  />
+                </div>
+              </div>
+            </div>
+
             <div className="py-8">
               <Bar data={data} options={options} />
             </div>
 
             <h1 className=" text-2xl font-bold py-4">Transaction</h1>
-            <div className="" style={{ height: 400, width: "100%" }}>
+            <div style={{ height: 400, width: "100%" }}>
               <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                  pagination: {
-                    paginationModel: { page: 0, pageSize: 5 },
+                rows={getTable.map((transaction, index) => ({
+                  id: index,
+                  ...transaction,
+                }))}
+                columns={[
+                  { field: "f_name", headerName: "First Name", flex: 1 },
+                  { field: "l_name", headerName: "Last Name", flex: 1 },
+                  { field: "price", headerName: "Price", flex: 1 },
+                  {
+                    field: "payment_date",
+                    headerName: "Payment Date",
+                    flex: 1,
                   },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
+                  {
+                    field: "isrefund",
+                    headerName: "Refund Status",
+                    flex: 1,
+                    valueGetter: (params) =>
+                      params.row.isrefund ? "Refunded" : "Not Refunded",
+                  },
+                ]}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
               />
             </div>
             <div className="flex justify-between mt-1 py-4">
